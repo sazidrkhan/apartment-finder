61668c73a81f8631ca33a961aa83ad6d583add74
SazidRKhan <md.sazidrkhan@gmail.com>
2024-05-05 01:30:27 +0600

    Completed the ApartmentFinder.java file.

--- Run.java
+++ Run.java
@@ -1,5 +1,8 @@
-public class Run{
-    public static void main(String[] args){
-        new Nothing();
-    }
+import classes.*;
+
+public class Run {
+	public static void main(String[] args) {
+		new ApartmentFinder();
+	}
+
 }
---
+++ classes/ApartmentFinder.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class ApartmentFinder {
+    public ApartmentFinder() {
+        System.out.println("ApartmentFinder constructor");
+    }
+}
