2a81e38504ab1dd039bf9e2f59c27d634d98bede
SazidRKhan <md.sazidrkhan@gmail.com>
2024-05-10 23:07:14 +0600

    Modified

--- applicationpackage/User.java
+++ applicationpackage/User.java
@@ -20,8 +20,8 @@ public class User  {    // This class is used to store user information and regi
     }
 
     public boolean registerUser() { // Method to register the user
-        try (FileWriter fileWriter = new FileWriter("database\\UserData.txt", true)) {  // Try block to write user information to the file UserData.txt in the database folder 
-            fileWriter.write(name + "," + username + "," + age + "," + gender + "," + nidOrBid + "," + password + System.lineSeparator());    // Writing user information to the file in a specific format to read later on easily 
+        try (FileWriter fileWriter = new FileWriter("database/UserData.txt", true)) {  // Try block to write user information to the file UserData.txt in the database folder 
+            fileWriter.write(name + "_" + username + "_" + age + "_" + gender + "_" + nidOrBid + "_" + password + System.lineSeparator());    // Writing user information to the file in a specific format to read later on easily 
             return true;    // Returning true if the user is successfully registered 
         } catch (IOException e) {   // Catch block to catch any exceptions that occur while writing to the file 
             e.printStackTrace();    // Printing the stack trace of the exception 
