ac6ad7b6748e84ed82e400828e3f0401e9d90c1d
SazidRKhan <md.sazidrkhan@gmail.com>
2024-05-11 13:57:43 +0600

    A file was renamed.

--- applicationpackage/UserDashboard.java
+++ applicationpackage/UserDashboard.java
@@ -2,8 +2,12 @@ package applicationpackage;
 
 public class UserDashboard {
 
-    public UserDashboard(UserLogin userLogin) {
-        //TODO Auto-generated constructor stub
+    public UserDashboard(String name) {
+        System.out.println("User Dashboard Constructor");
     }
 
+    public void setVisible(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVisible'");
+    }
 }
