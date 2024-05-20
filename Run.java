fbcc68bf54c447b209587f8d2b219057866ee398
SazidRKhan <md.sazidrkhan@gmail.com>
2024-05-20 22:52:27 +0600

    Final push!

--- Run.java
+++ Run.java
@@ -1,5 +1,6 @@
-import applicationpackage.*;	
+import applicationpackage.*;	// Importing the package containing the main classes of the application
 
+// Main class to kick off the application
 public class Run {	
 	public static void main(String[] args) {	 
 		new ApartmentFinder();	 
